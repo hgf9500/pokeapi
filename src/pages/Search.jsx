db4470eef75b59962af6a4d99c9f65cd25acbbca
이원주 <hgf9500@gmail.com>
2025-11-17 11:43:16 +0900

    fix: Search.jsx 빌드 경로 오류 최종 해결 및 확장자 명시

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { fetchPokemonList } from '../api/pokeapi';
+import { fetchPokemonList } from '../api/pokeapi.js';
  
 
 
